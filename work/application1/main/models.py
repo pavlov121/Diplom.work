0cf7f31d28875ab4bb4de0ac746b1bf1da832c25
Pavlov <pavlovalexey2003@gmail.com>
2025-10-20 22:52:20 +0300

    update

--- work/application1/main/models.py
+++ work/application1/main/models.py
@@ -1,68 +1,50 @@
 from django.db import models
 
 
-# class Main2(models.Model):
-#     title = models.CharField(max_length=200)
-#     title2 = models.CharField(max_length=200)
-#     body = models.TextField(blank=True)
-#
-#     def __str__(self):
-#         return self.title
-#
-#
-# class Main(models.Model):
-#     title = models.CharField(max_length=200)  # Название Соревнования
-#     description = models.TextField(blank=True)  # Описание
-#     image = models.ImageField(upload_to="main/%Y/%m/%d/", blank=True,
-#                               default="run.jpg")  # Изображение (создание папки с датой создания)
-#     demo_link = models.CharField(max_length=2000, blank=True)  # Подробная информация о марафоне
-#     city = models.ManyToManyField('City', blank=True)  # Связь многие ко многим: К одному марафону, несколько городов
-#     uch = models.IntegerField(default=0)  # Количество участников
-#     created = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.title
-#
-#
-# class City(models.Model):  # Города в которых проводится данный марафон
-#     name = models.CharField(max_length=200)
-#     created = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.name
-
-
 class Room(models.Model):
-    title = models.CharField(max_length=200)  # Для определения
-    address = models.CharField(max_length=200)  # Адрес
-    money = models.CharField(max_length=200)  # Стоимость
-    Bedrooms = models.IntegerField(default=0)  # Спальная комната
-    Bathrooms = models.IntegerField(default=0)  # Ванная комната
-    Area = models.CharField(max_length=200)  # Площадь
-    Floor = models.IntegerField(default=0)  # Этаж
-    Parking = models.CharField(max_length=200)  # Парковка
+    name = models.CharField(max_length=256, verbose_name="Название квартира")
+    address = models.CharField(max_length=200, verbose_name="Адрес")
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name="Цена")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    short_description = models.CharField(max_length=100, blank=True, verbose_name="Краткое описание")
+    Area = models.CharField(max_length=200, verbose_name='Площадь квартиры')
+    Floor = models.IntegerField(default=0, verbose_name='Этаж')
+    year = models.IntegerField(default=0, verbose_name='Год постройки')
+    Parking = models.CharField(max_length=200, verbose_name='Парковка')
     image = models.ImageField(upload_to="main/%Y/%m/%d/", blank=True,
-                              default="run.jpg")  # Изображение (создание папки с датой создания)
-    Company = models.ManyToManyField('Company', blank=True)  # К одной квартире много компаний
+                              default="run.jpg",
+                              verbose_name="Изображение квартиры")  # Изображение (создание папки с датой создания)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE,
+                                verbose_name="Компания")  # К одной квартире много компаний
 
     def __str__(self):
-        return self.title
+        return self.name
+
+    class Meta:
+        verbose_name = 'квартира'
+        verbose_name_plural = 'квартиры'
 
 
 class Company(models.Model):
-    title = models.CharField(max_length=200)  # Название компании
-    description = models.TextField(blank=True)  # Описание
+    title = models.CharField(max_length=200, verbose_name='Название компании')
+    description = models.TextField(blank=True, verbose_name='Описание')
     image = models.ImageField(upload_to="main/%Y/%m/%d/", blank=True,
-                              default="run.jpg")  # Изображение (создание папки с датой создания)
-    demo_link = models.CharField(max_length=2000, blank=True)  # Подробная информация
+                              default="run.jpg",
+                              verbose_name='Логотип компании')  # Изображение (создание папки с датой создания)
+    demo_link = models.CharField(max_length=2000, blank=True, verbose_name='Ссылка')  # Подробная информация
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'компания'
+        verbose_name_plural = 'компании'
 
 
 class Eco(models.Model):
     image2 = models.ImageField(upload_to="main/%Y/%m/%d/", blank=True,
                                default="run.jpg")
     category = models.CharField(max_length=200)
+
 
 
